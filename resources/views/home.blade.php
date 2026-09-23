95ae836623b389334587bdccd8e980df1403a9f9
nael <nael@email.com>
2026-09-23 17:22:01 +0200

    end of 2nd session

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+</head>
+<body>
+    <header>
+    </header>
+    <main>
+        <h1>Weather App</h1>
+
+        <nav>
+            <a href="{{ route('login') }}">login</a>
+        </nav>
+        
+        
+    </main>
+</body>
+</html>
